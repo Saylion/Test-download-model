b7e5d97cef4ca0b09244e6c14efdc520504ec184
Saylion <126657247+Saylion@users.noreply.github.com>
2023-10-08 06:50:48 +0700

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
 
     pretrained_v1_model_names = ['D32k.pth', 'D42k.pth', 'D48k.pth', 'G32k.pth', 'G42k.pth', 'G48k.pth', 'f0D32k.pth', 'f0D42k.pth' 'f0D48k.pth', 'f0G32k.pth', 'f0G42k.pth', 'f0G48k.pth']
-    for model in pretrained_v1_model_name:
+    for model in pretrained_v1_model_names:
         print(f'Downloading {model}...')
         dl_model(PRETRAINED_V1_DOWNLOAD_LINK, model, pretrained_v1_models_dir)
 
